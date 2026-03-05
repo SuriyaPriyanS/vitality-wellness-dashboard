9aa049b8bb2e1be7e02dd16b51308e619a28dcaa
Suriya Priyan <suriyapriyan0506@gmail.com>
2026-03-05 17:30:46 +0530

    updated

--- services/api.ts
+++ services/api.ts
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
-const API_BASE_URL =   'http://localhost:4000/api';
+const API_BASE_URL =   'https://backendserver-9fvo.onrender.com/api';
+
+
 
 // Create axios instance
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
-  timeout: 10000,
+  
   headers: {
     'Content-Type': 'application/json',
   },
